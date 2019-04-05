ebc13c1f84194985ce29a42ba59f766ca09a2567
Lusiné Keshishyan <tadouchiha@gmail.com>
2019-04-05 00:45:45 -0400

    main driver file

---
+++ main.lisp
@@ -0,0 +1,17 @@
+; path as seen from the compiler folder
+(load "../content/lists")
+(load "../content/set")
+(load "../content/math")
+(load "../content/required")
+
+; main
+(defun main ()
+  (print "list functions:")
+  (list_)
+  (print "set functions:")
+  (set)
+  (print "math functions:")
+  (math)
+  (print "required functions:")
+  (required)
+)
